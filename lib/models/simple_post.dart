955412785aec6e98938ea5a64c4390287099f682
Sehyuk Ahn <shyukahn@gmail.com>
2024-07-08 21:10:50 +0900

    feat: made community page sample

---
+++ lib/models/simple_post.dart
@@ -0,0 +1,7 @@
+class SimplePost {
+  final String title;
+  final String summary;
+  final int comments;
+
+  SimplePost({required this.title, required this.summary, required this.comments});
+}
--- lib/pages/community_page.dart
+++ lib/pages/community_page.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:serverapp/widgets/post_list_view.dart';
+
+import '../models/simple_post.dart';
 
 class CommunityPage extends StatefulWidget {
   const CommunityPage({super.key});
@@ -8,12 +11,32 @@ class CommunityPage extends StatefulWidget {
 }
 
 class _CommunityPageState extends State<CommunityPage> {
+  final List<SimplePost> _simplePosts = [];
+
+  void _writePost() {
+
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    for (int i = 0; i < 40; i++) {
+      _simplePosts.add(SimplePost(title: 'Title $i', summary: 'Summary $i', comments: i % 2));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Page 1',
-      style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+    return Scaffold(
+      body: Center(
+        child: PostListView(simplePosts: _simplePosts)
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _writePost,
+        shape: const CircleBorder(),
+        child: const Icon(Icons.create),
+      ),
     );
   }
 }
