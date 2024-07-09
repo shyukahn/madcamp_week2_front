3ce30faca49e8fbf868284e0dab8f370651d5cbe
Sehyuk Ahn <shyukahn@gmail.com>
2024-07-09 17:57:43 +0900

    feat: get past questions from server

--- lib/pages/problem_page.dart
+++ lib/pages/problem_page.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:serverapp/pages/gemini_review_page.dart';
+import 'package:http/http.dart' as http;
 
 import '../models/gemini_response.dart';
 
@@ -15,6 +20,7 @@ class ProblemPage extends StatefulWidget {
 class _ProblemPageState extends State<ProblemPage> with AutomaticKeepAliveClientMixin<ProblemPage> {
   final imagePicker = ImagePicker();
   bool _isLoading = true;
+  final _questionUrl = '${dotenv.env['baseUrl']}my_question/my_page/user_question';
   List<GeminiResponse>? _pastQuestions;
 
   void _showGeminiReviewPage(ImageSource source) async {
@@ -31,22 +37,65 @@ class _ProblemPageState extends State<ProblemPage> with AutomaticKeepAliveClient
   bool get wantKeepAlive => true;
 
   @override
+  void initState() {
+    _getPastQuestions();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     super.build(context);
     return Scaffold(
-      body: const Center(
-        child: Text('Search Something!'),
-      ),
+      body: _showPastQuestions(),
       floatingActionButtonLocation: ExpandableFab.location,
       floatingActionButton: _floatingActionButton(),
     );
   }
 
+  void _getPastQuestions() async {
+    final user = await UserApi.instance.me();
+    final pastQuestionsResponse = await http.get(Uri.parse('$_questionUrl?kakao_id=${user.id}'));
+    if (pastQuestionsResponse.statusCode == 200) {
+      final List<Map<String, String>> jsonList = List<Map<String, String>>.from(jsonDecode(pastQuestionsResponse.body));
+      _pastQuestions = jsonList.map(
+        (Map<String, String> jsonMap) {
+          return GeminiResponse(
+            content: jsonMap['content']!,
+            answer: jsonMap['answer']!,
+            solution: '',
+          );
+        }
+      ).toList();
+    }
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
   Widget _showPastQuestions() {
     if (_isLoading) {
-      return CircularProgressIndicator();
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    } else if (_pastQuestions == null) {
+      return const Center(
+        child: Text('오류가 발생했습니다')
+      );
     } else {
-      return Text('Search Something!');
+      return ListView.separated(
+        itemCount: _pastQuestions!.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Column(
+            children: [
+              Text(_pastQuestions![index].content),
+              Text(_pastQuestions![index].answer),
+            ],
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return Divider();
+        },
+      );
     }
   }
 
